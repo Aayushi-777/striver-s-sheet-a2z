3225e0878e90c165f66351953d33771b0a634dbb
Aayushi Vinod <aayushi.vinod2507@gmail.com>
2026-09-06 16:07:39 +0530

    Changes

--- Bit Manipulation/Divisors/main.py
+++ Bit Manipulation/Divisors/main.py
@@ -4,7 +4,7 @@ class Solution:
         for i in range(1, int(n**0.5)+1):
             if n%i==0:
                 res.append(i)
-                if i!=n//i:
+                if n//i!=i:
                     res.append(n//i)
         res.sort()
         return res
