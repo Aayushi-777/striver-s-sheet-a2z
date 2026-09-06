b8c70c6423ca1725cbffe413c8c85c360295a9af
Aayushi Vinod <aayushi.vinod2507@gmail.com>
2026-09-06 16:33:30 +0530

    Changes

--- Patterns/Pattern11/main.py
+++ Patterns/Pattern11/main.py
@@ -1,11 +1,11 @@
 class Solution:
     def pattern(self, n):
-        for i in range(n):
+        for i in range(1, n+1):
             if i%2==0:
-                start=1
-            else:
                 start=0
-            for j in range(i+1):
+            else:
+                start=1
+            for j in range(1, i+1):
                 print(start, end=" ")
                 start=1-start
             print()
